5bb1caec7aa33b93fb857116c865dee36ff2dd0d
katarina <golbang.k@gmail.com>
2015-03-20 21:13:54 +0100

    fix errors

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,25 @@
 class ItemsController < ApplicationController
 
   def create
-    backlog = Backlog.find(params[:backlog_id])
-    @item = Item.new(item_params.merge(backlog: backlog))
+    @backlog = Backlog.find(params[:backlog_id])
+    @item = Item.new(item_params.merge(backlog: @backlog))
     if @item.save
-      flash[:notice] = "Item '#{@item.name}' saved successfully.'"
-      redirect_to backlog_path(@item.backlog)
+      flash[:notice] = "Item '#{@item.name}' saved successfully."
+      redirect_to backlog_path(@backlog)
     else
+      flash[:error] = "Item '#{@item.name}' could not be created."
       render 'backlogs/show'
     end
   end
 
   def update
     @item = Item.find(params[:id])
+    @backlog = @item.backlog
     if @item.update(item_params)
       flash[:notice] = "Item '#{@item.name}' updated successfully."
-      redirect_to backlog_path(@item.backlog)
+      redirect_to backlog_path(@item.backlog, item: params[:id])
     else
+      flash[:error] = "Item '#{@item.name}' could not be updated."
       render 'backlogs/show'
     end
   end
