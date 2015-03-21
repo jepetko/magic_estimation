c2e92df75e0a9e2354c545200f92b05abb99dc15
katarina <golbang.k@gmail.com>
2015-03-21 21:37:54 +0100

    show estimator

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,15 @@
 class Item < ActiveRecord::Base
+
+  attr_accessor :the_initial_estimator
+
   validates :name, :description, :creator, presence: true
   belongs_to :backlog
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :estimations
   has_many :estimators, through: :estimations
+
+  def initial_estimator
+    # note: there should be only one estimator with initial = true for an item!
+    self.the_initial_estimator ||= self.estimators.where('estimations.initial' => true).limit(1).first
+  end
 end
