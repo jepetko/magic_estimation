442bc8215096254c3ea6a95fecf6b79ebd66476b
katarina <golbang.k@gmail.com>
2015-03-22 13:37:44 +0100

    scopes for items

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,8 +8,12 @@ class Item < ActiveRecord::Base
   has_many :estimations
   has_many :estimators, through: :estimations
 
+  scope :for_backlog, ->(backlog) { where(backlog: backlog) }
+  scope :for_backlog_and_estimator_where_initial, ->(backlog,estimator,initial) { where(backlog: backlog).joins(:estimations).where(user_id: estimator.id,:'estimations.initial' => initial) }
+
   def initial_estimator
     # note: there should be only one estimator with initial = true for an item!
     self.the_initial_estimator ||= self.estimators.where('estimations.initial' => true).limit(1).first
   end
+
 end
