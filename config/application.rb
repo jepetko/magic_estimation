297f2efdfd6209ce525b2108bf6c687c48c3fa8c
katarina <golbang.k@gmail.com>
2015-04-07 19:08:33 +0200

    dont connect to database on precompile phase

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module MagicEstimation
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # heroku issue: http://stackoverflow.com/questions/7430578/how-to-universally-skip-database-touches-when-precompiling-assets-on-heroku
+    config.assets.initialize_on_precompile = false
   end
 end
